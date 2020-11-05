ba35f36a8ce2f205bf869d387116aea049f6d220
Edinardo Freitas Filho <edinardofreitas@gmail.com>
2020-11-05 15:41:15 -0300

    Updated PrintBoard

--- ChessSystem_Console/Program.cs
+++ ChessSystem_Console/Program.cs
@@ -8,13 +8,25 @@ namespace ChessSystem_Console
     {
         static void Main(string[] args)
         {
-            ChessPosition position = new ChessPosition('c', 7);
+            try
+            {
+                Board board = new Board(8, 8);
 
-            Console.WriteLine(position);
-            Console.WriteLine(position.ToPosition());
+                board.PlacePiece(new Tower(board, Color.Black), new Position(0, 0));
+                board.PlacePiece(new Tower(board, Color.Black), new Position(1, 3));
+                board.PlacePiece(new King(board, Color.Black), new Position(0, 2));
 
+                board.PlacePiece(new Tower(board, Color.White), new Position(6, 3));
+                board.PlacePiece(new Tower(board, Color.White), new Position(7, 4));
+                board.PlacePiece(new King(board, Color.White), new Position(6, 0));
+
+                UI.PrintBoard(board);
+            }
+            catch (BoardException e)
+            {
+                Console.WriteLine(e.Message);
+            }
             Console.ReadLine();
-
         }
     }
 }
--- ChessSystem_Console/UI.cs
+++ ChessSystem_Console/UI.cs
@@ -9,18 +9,37 @@ namespace ChessSystem_Console
         {
             for (int i = 0; i < board.Rows; i++)
             {
+                Console.Write(8 - i + " ");
+
                 for (int j = 0; j < board.Columns; j++)
                 {
+                    
                     if (board.Piece(i, j) == null)
                     {
                         Console.Write("- ");
                     }
                     else
                     {
-                        Console.Write(board.Piece(i, j) + " ");
+                        UI.PrintPiece(board.Piece(i, j));
+                        Console.Write(" ");
                     }
                 }
                 Console.WriteLine();
+            }
+            Console.WriteLine("  a b c d e f g h");
+        }
+
+        public static void PrintPiece(Piece piece)
+        {
+            if (piece.Color == Color.White)
+            {
+                Console.Write(piece);
+            } else
+            {
+                ConsoleColor aux = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write(piece);
+                Console.ForegroundColor = aux;
             }
         }
     }
