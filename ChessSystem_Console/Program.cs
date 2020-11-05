0be2e99523f68e4c8e7d02f9d8b2579c92c22e89
Edinardo Freitas Filho <edinardofreitas@gmail.com>
2020-11-05 13:43:51 -0300

    Project Created

---
+++ ChessSystem_Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ChessSystem_Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
