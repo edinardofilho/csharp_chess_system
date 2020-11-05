7ddd6c2f80ae165610da3168ed158f626b4fb033
Edinardo Freitas Filho <edinardofreitas@gmail.com>
2020-11-05 15:21:57 -0300

    Created ChessPosition

---
+++ ChessSystem_Console/Chess/ChessPosition.cs
@@ -0,0 +1,26 @@
+﻿using Boardgame;
+
+namespace Chess
+{
+    public class ChessPosition
+    {
+        public char Column { get; set; }
+        public int Row { get; set; }
+
+        public ChessPosition(char column, int row)
+        {
+            Column = column;
+            Row = row;
+        }
+
+        public Position ToPosition()
+        {
+            return new Position(8 - Row, Column - 'a');
+        }
+
+        public override string ToString()
+        {
+            return Column.ToString() + Row.ToString();
+        }
+    }
+}
--- ChessSystem_Console/Program.cs
+++ ChessSystem_Console/Program.cs
@@ -8,22 +8,13 @@ namespace ChessSystem_Console
     {
         static void Main(string[] args)
         {
-            Board board = new Board(8, 8);
-            try
-            {
-                board.PlacePiece(new Tower(board, Color.Black), new Position(0, 0));
-                board.PlacePiece(new Tower(board, Color.Black), new Position(1, 3));
-                board.PlacePiece(new King(board, Color.Black), new Position(2, 4));
-                board.PlacePiece(new Tower(board, Color.Black), new Position(2, 9));
+            ChessPosition position = new ChessPosition('c', 7);
 
-                UI.PrintBoard(board);
+            Console.WriteLine(position);
+            Console.WriteLine(position.ToPosition());
 
-                Console.ReadLine();
-            }
-            catch (BoardException e)
-            {
-                Console.WriteLine(e.Message);
-            }          
+            Console.ReadLine();
+
         }
     }
 }
